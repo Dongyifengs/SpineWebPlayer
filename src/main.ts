c9ee0672733ecc136ecb87e1712257c494a1aa6b
wzp <55318422+Wzp-2008@users.noreply.github.com>
2024-01-06 03:32:21 +0000

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from "element-plus";
+import 'element-plus/dist/index.css'
+// https://github.com/cambridgejames/NodeEditor3-vue/blob/develop/docs/guide/quick-start.md
+import NodeEditor from "node-editor3-vue";
+import "node-editor3-vue/lib/bundle.min.css";
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(NodeEditor);
+app.mount("#app");
